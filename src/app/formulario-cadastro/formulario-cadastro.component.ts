48f634fc8b7e3e42292ba6ca6e182497bbd09f51
Allan <allans@brq.com>
2022-02-23 10:38:47 -0300

    fix: "formulario-cadastro" ajuste da identação e apresentação da data de nascimento.

--- src/app/formulario-cadastro/formulario-cadastro.component.ts
+++ src/app/formulario-cadastro/formulario-cadastro.component.ts
@@ -5,14 +5,12 @@ import { switchMap } from 'rxjs/operators';
 import { FormGroup } from '@angular/forms';
 import { Clientes } from '../Models/clientes';
 
-
 @Component({
   selector: 'app-formulario-cadastro',
   templateUrl: './formulario-cadastro.component.html',
-  styles: []
+  styles: [],
 })
 export class FormularioCadastroComponent implements OnInit {
-
   contatoForm: FormGroup;
   submittingForm: boolean = false;
   currentAction: string;
@@ -22,74 +20,77 @@ export class FormularioCadastroComponent implements OnInit {
   constructor(
     private _clientesService: clientesService,
     private route: ActivatedRoute,
-    private router: Router,
-  ) { }
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.clientes = new Clientes();
     this.setCurrentAction();
   }
 
-  ngAfterContentChecked(){
+  ngAfterContentChecked() {
     this.atualizarPageTitle();
   }
 
   private setCurrentAction() {
-    if(this.route.snapshot.url[1].path == "novo"){
-      this.currentAction = "new";
-    }
-    else
-    {
-      this.currentAction = "edit";
+    if (this.route.snapshot.url[1].path == 'novo') {
+      this.currentAction = 'new';
+    } else {
+      this.currentAction = 'edit';
       this.carregarCadastro();
     }
   }
 
   submitForm(cadastroForm) {
-      this.submittingForm = true;
-    if (this.currentAction == "new") {
+    this.submittingForm = true;
+    if (this.currentAction == 'new') {
       this.criarCadastro(cadastroForm);
-    }
-    else {
+    } else {
       this.atualizarCadastro(cadastroForm);
     }
   }
-   private carregarCadastro() {
-    if(this.currentAction == "edit"){
-      this.route.paramMap.pipe(
-
-        switchMap(params => this._clientesService.buscarPorId(+params.get("id")))
-      )
-      .subscribe(
-        (c)=>{
-          this.clientes = c;
-        },
-        (error) => alert('Ocorreu um erro no servidor, tente novamente.')
-      )
+  private carregarCadastro() {
+    if (this.currentAction == 'edit') {
+      this.route.paramMap
+        .pipe(
+          switchMap((params) =>
+            this._clientesService.buscarPorId(+params.get('id'))
+          )
+        )
+        .subscribe(
+          (c) => {
+            this.clientes = c;
+          },
+          (error) => alert('Ocorreu um erro no servidor, tente novamente.')
+        );
     }
   }
 
   private criarCadastro(cadastroForm) {
-    const clienteTela: Clientes = Object.assign(new Clientes(), cadastroForm.value);
-       this._clientesService.gravar(clienteTela)
-       .subscribe(
-          () => this.cadastroGravadoComSucesso(),
-          error => this.erroAoGravarCadastro(error)
-       )
+    const clienteTela: Clientes = Object.assign(
+      new Clientes(),
+      cadastroForm.value
+    );
+    this._clientesService.gravar(clienteTela).subscribe(
+      () => this.cadastroGravadoComSucesso(),
+      (error) => this.erroAoGravarCadastro(error)
+    );
   }
 
   private atualizarCadastro(cadastroForm) {
-    const clienteTela: Clientes = Object.assign(new Clientes(), cadastroForm.value);
+    const clienteTela: Clientes = Object.assign(
+      new Clientes(),
+      cadastroForm.value
+    );
 
-    this._clientesService.atualizar(clienteTela)
-      .subscribe(
-        () => this.cadastroGravadoComSucesso(),
-        error => this.erroAoGravarCadastro(error)
-      )
+    this._clientesService.atualizar(clienteTela).subscribe(
+      () => this.cadastroGravadoComSucesso(),
+      (error) => this.erroAoGravarCadastro(error)
+    );
   }
 
   private cadastroGravadoComSucesso() {
-    this.router.navigateByUrl("cadastro")
+    this.router.navigateByUrl('cadastro');
   }
 
   private erroAoGravarCadastro(error) {
@@ -97,11 +98,10 @@ export class FormularioCadastroComponent implements OnInit {
   }
 
   private atualizarPageTitle() {
-    if(this.currentAction == "new")
-      this.pageTitle = "Novo Cadastro";
-    else{
-      const cadastroNome = this.clientes.nome || ""
-      this.pageTitle = "Editando Cadastro: " + cadastroNome;
+    if (this.currentAction == 'new') this.pageTitle = 'Novo Cadastro';
+    else {
+      const cadastroNome = this.clientes.nome || '';
+      this.pageTitle = 'Editando Cadastro: ' + cadastroNome;
     }
   }
 }
